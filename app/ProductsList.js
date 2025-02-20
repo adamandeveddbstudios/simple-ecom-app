c4f50adac7b7bfe171cdc154620627937e9a4738
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2025-02-20 20:31:49 +0530

    product list page modified

--- app/ProductsList.js
+++ app/ProductsList.js
@@ -4,6 +4,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
+export const dynamic = 'force-dynamic';
+
 export default function ProductsList({products, initialCartProducts}) {
 
     const [cartProducts, setCartProducts] = useState(initialCartProducts);
